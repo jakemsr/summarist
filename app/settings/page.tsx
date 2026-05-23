a068a2a2d910e5af121183a7eac9eb135065e474
jakemsr <jakemsr@yahoo.com>
2026-05-23 06:56:06 -0700

    Add library and settings layouts
    - prepare the library and settings pages by adding layouts to their
      folders

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,22 +1,10 @@
-import Search from "@/components/search/Search"
-import Sidebar from "@/components/sidebar/Sidebar"
 
 
 const Settings = () => {
   return (
-    <div className="wrapper">
-      <Search />
-      <div className="sidebar__overlay sidebar__overlay--hidden"></div>
-      <Sidebar />
-
-      <div className="row">
-        <div className="container">
-
-          Settings
-
-        </div>
-      </div>
-    </div>
+    <>
+      Settings
+    </>
   )
 }
 
