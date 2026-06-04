2d3880ba6ce296351c4e2f5d596c9f81f45b2251
jakemsr <jakemsr@yahoo.com>
2026-06-03 19:04:57 -0700

    Implement the search component
    - Debounce the input/serach cycle by wating 500ms between last input
      change and starting the data fetch.
    - Display 4 skeleton book entries while data is fetching.
    - Create a SearchBook component that displays a single book. This makes
      the logic around displaying the audio duration much easier.

---
+++ components/search/SearchBook.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRef, useState } from "react";
+import Link from "next/link";
+import { LuClock3 } from "react-icons/lu";
+import { Book } from "@/lib/types";
+import styles from "./search.module.css";
+
+
+const SearchBook = ({ book }: { book: Book }) => {
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const [duration, setDuration] = useState<string>("0:00");
+
+  const onLoadedMetadata = () => {
+    if (audioRef.current) {
+      let seconds = Math.floor(audioRef.current.duration);
+      const minutes = Math.floor(seconds / 60);
+      seconds = seconds % 60;
+      setDuration(`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`);
+    }
+  };
+
+
+  return (
+    <Link href={`/book/${book.id}`} className={styles.bookLink}>
+      <audio
+        src={book.audioLink}
+        ref={audioRef}
+        onLoadedMetadata={onLoadedMetadata}
+      />
+      <figure className={styles.bookImageWrapper}>
+        <img src={book.imageLink} alt="" className={styles.bookImage} />
+      </figure>
+      <div>
+        <div className={styles.bookTitle}>
+          {book.title}
+        </div>
+        <div className={styles.bookAuthor}>
+          {book.author}
+        </div>
+        <div className={styles.bookDuration}>
+          <div className={styles.bookDetails}>
+            <div className={styles.bookDetailsIcon}>
+              <LuClock3 />
+            </div>
+            <div className={styles.bookDetailsText}>
+              {duration}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default SearchBook
