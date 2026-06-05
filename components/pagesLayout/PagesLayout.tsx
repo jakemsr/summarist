daa414ff47646733506b0e82a8d2c337e1b389c4
jakemsr <jakemsr@yahoo.com>
2026-06-04 17:50:25 -0700

    Make layouts know if they are the player page
    - The player page has a slightly different layout than the other user
      pages. It isn't wrapper in "row" and "container" divs.
    - The sidebar is also different for the player page as it has a font
      size chooser.
    - To deal with these differences, pass a "player" prop to PagesLayout,
      and drill that same prop to the Sidebar. PagesLayout then knows to not
      use the "row" and "container" divs for the player page, and Sidebar
      will know it's rendering for the player page. This commit only adds
      the prop to the Sidebar; implementation of changes coming in later
      commits. This commit is just for the layout changes.

--- components/pagesLayout/PagesLayout.tsx
+++ components/pagesLayout/PagesLayout.tsx
@@ -6,7 +6,7 @@ import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { closeSidebar, SidebarState } from "@/lib/features/sidebar/sidebarSlice";
 import styles from "./pagesLayout.module.css";
 
-const PagesLayout = ({ children }: {children: React.ReactNode}) => {
+const PagesLayout = ({ player, children }: { player: boolean, children: React.ReactNode }) => {
 
   const dispatch = useAppDispatch();
   const sidebar: SidebarState = useAppSelector(state => state.sidebar);
@@ -17,16 +17,24 @@ const PagesLayout = ({ children }: {children: React.ReactNode}) => {
       <div
         className={`${styles.sidebarOverlay} ${sidebar.isOpen ? "" : styles.sidebarOverlayHidden}`}
         onClick={() => dispatch(closeSidebar())}
-        >
+      >
       </div>
-      <Sidebar />
-      <div className="row">
-        <div className="container">
 
+      <Sidebar player={player} />
+
+      {player ? (
+        <>
           {children}
+        </>
+      ) : (
+        <div className="row">
+          <div className="container">
 
+            {children}
+
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }
